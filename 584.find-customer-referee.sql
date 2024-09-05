81f16d8d32f60dd9bf16332019bc87383e1371b0
mtriSE <mtri0162@gmail.com>
2024-09-05 21:00:51 +0700

    (chore) explain 584

--- 584.find-customer-referee.sql
+++ 584.find-customer-referee.sql
@@ -6,6 +6,7 @@
 
 -- @lc code=start
 # Write your MySQL query statement below
+# In SQL, NULL represents the absence of a value, meaning it is not equivalent to any value, including an empty string or zero. A comparison like field != value will not return rows where field is NULL because NULL is neither equal nor unequal to any value. In other words, SQL considers comparisons involving NULL to be unknown
 select name
 from Customer
 where (referee_id <> 2) or (referee_id is null);
