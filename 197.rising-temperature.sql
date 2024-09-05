5fbe38344b3c823e3241c0eb2fdbc68fd4e83d42
mtriSE <mtri0162@gmail.com>
2024-09-06 00:15:33 +0700

    197. rising temperature

---
+++ 197.rising-temperature.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode id=197 lang=mysql
+--
+-- [197] Rising Temperature
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select w1.id
+from Weather w1, Weather w2
+where DATEDIFF(w1.recordDate,w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+-- @lc code=end
+
